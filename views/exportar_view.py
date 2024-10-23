9154815e9b444831d5871b4ef5c1b666f06cea2b
bbnadev <nicolas.bahamondeb@gmail.com>
2024-10-22 23:16:53 -0300

    Refactorizar ExportarView para eliminar método innecesario y agregar métodos para exportar a Excel y PDF

--- views/exportar_view.py
+++ views/exportar_view.py
@@ -5,10 +5,7 @@ from config.database import db_config
 
 class ExportarView:
     def __init__(self):
-        pass
-
-    def exportar_excel(self):
-        query = """
+        self._query = """
 SELECT 
     e.id AS empleado_id,
     e.nombre AS empleado_nombre,
@@ -27,12 +24,46 @@ LEFT JOIN
 LEFT JOIN 
     Proyecto p ON rt.id_proyecto = p.id
 """
-
-        db_connection_str = f'mysql+mysqlconnector://{db_config['user']}:{
+        self._connector_str = f'mysql+mysqlconnector://{db_config['user']}:{
             db_config['password']}@{db_config['host']}/{db_config["database"]}'
+
+    def query(self):
+        return self._query
+
+    def get_connector_str(self):
+        return self._connector_str
+
+    def create_connection(self):
+        db_connection_str = self.get_connector_str()
         db_connection = create_engine(db_connection_str)
+        return db_connection
+
+    def exportar_excel(self):
+
+        conn = self.create_connection()
 
         df_informe = pd.read_sql(
-            query, con=db_connection)
+            self.query(), con=conn)
         df_informe.to_excel('informes/informe_empleados.xlsx', index=False)
         print("Informe de Excel generado exitosamente.")
+
+    def exportar_pdf(self):
+        import matplotlib.pyplot as plt
+        from matplotlib.backends.backend_pdf import PdfPages
+
+        conn = self.create_connection()
+
+        df = pd.read_sql(
+            self.query(), con=conn)
+
+        fig, ax = plt.subplots(figsize=(12, 4))
+        ax.axis('tight')
+        ax.axis('off')
+        ax.table(cellText=df.values,
+                 colLabels=df.columns)
+        ax.set_title('Informe de empleados')
+
+        pp = PdfPages("informes/informe_empleados.pdf")
+        pp.savefig(fig, bbox_inches='tight')
+        pp.close()
+        print("Informe de PDF generado exitosamente.")
