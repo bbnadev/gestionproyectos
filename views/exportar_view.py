fa3ba7e9204d61ac3e7d5f0f792881e619cc8fa1
bbnadev <nicolas.bahamondeb@gmail.com>
2024-10-23 22:38:54 -0300

    Refactorizar ExportarView para eliminar método innecesario y agregar métodos para exportar a Excel y PDF

--- views/exportar_view.py
+++ views/exportar_view.py
@@ -1,49 +1,13 @@
 import pandas as pd
-from sqlalchemy import create_engine
-from config.database import db_config
-
+from controllers.exportar_controller import ExportarController
 
 class ExportarView:
     def __init__(self):
-        self._query = """
-SELECT 
-    e.id AS empleado_id,
-    e.nombre AS empleado_nombre,
-    e.rut AS empleado_rut,
-    d.nombre AS departamento_nombre,
-    p.nombre AS proyecto_nombre,
-    rt.fecha AS fecha_registro,
-    rt.horas_trabajadas,
-    rt.descripcion AS descripcion_registro
-FROM 
-    empleado e
-LEFT JOIN 
-    departamento d ON e.departamento_id = d.id
-LEFT JOIN 
-    RegistroTiempo rt ON e.id = rt.id_empleado
-LEFT JOIN 
-    Proyecto p ON rt.id_proyecto = p.id
-"""
-        self._connector_str = f'mysql+mysqlconnector://{db_config['user']}:{
-            db_config['password']}@{db_config['host']}/{db_config["database"]}'
-
-    def query(self):
-        return self._query
-
-    def get_connector_str(self):
-        return self._connector_str
-
-    def create_connection(self):
-        db_connection_str = self.get_connector_str()
-        db_connection = create_engine(db_connection_str)
-        return db_connection
+        self.controller = ExportarController()
 
     def exportar_excel(self):
 
-        conn = self.create_connection()
-
-        df = pd.read_sql(
-            self.query(), con=conn)
+        df = self.controller.exportar()
         df.to_excel('informes/informe_empleados.xlsx', index=False)
         print("Informe de Excel generado exitosamente.")
 
@@ -51,16 +15,15 @@ LEFT JOIN
         import matplotlib.pyplot as plt
         from matplotlib.backends.backend_pdf import PdfPages
 
-        conn = self.create_connection()
+        df = self.controller.exportar()
 
-        df = pd.read_sql(
-            self.query(), con=conn)
-
-        fig, ax = plt.subplots(figsize=(12, 4))
+        fig, ax = plt.subplots(figsize=(16.5, 11.7))
+        # A3 = 11.7 x 16.5 inches
+        # A4 = 8.3 x 11.7 inches
         ax.axis('tight')
         ax.axis('off')
         ax.table(cellText=df.values,
-                 colLabels=df.columns)
+                 colLabels=df.columns, loc="center")
         ax.set_title('Informe de empleados')
 
         pp = PdfPages("informes/informe_empleados.pdf")
