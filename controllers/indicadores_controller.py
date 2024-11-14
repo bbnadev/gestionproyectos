211f2df43281399d6420095002a68da36520f6c6
bbnadev <nicolas.bahamondeb@gmail.com>
2024-11-14 17:45:47 -0300

    Agregar clase IndicadorController para gestionar la conexión a la base de datos

---
+++ controllers/indicadores_controller.py
@@ -0,0 +1,13 @@
+from models.indicador import Indicador
+import mysql.connector
+from config.database import db_config
+
+
+class IndicadorController:
+    def __init__(self):
+        self.db_config = db_config
+
+    def conectar(self):
+        return mysql.connector.connect(**self.db_config)
+
+    # TODO
