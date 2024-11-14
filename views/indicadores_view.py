9695322f3fcd52a56a2bbaa4cd3d5cb793151e33
bbnadev <nicolas.bahamondeb@gmail.com>
2024-11-14 17:45:41 -0300

    Agregar clase IndicadoresView para gestionar la interacción con IndicadorController

---
+++ views/indicadores_view.py
@@ -0,0 +1,8 @@
+from controllers.indicadores_controller import IndicadorController, Indicador
+
+
+class IndicadoresView:
+    def __init__(self):
+        self.controller = IndicadorController()
+
+    # TODO
