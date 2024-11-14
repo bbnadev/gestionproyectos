abb22ab859e421a34327cef94e05897d94f35e3b
bbnadev <nicolas.bahamondeb@gmail.com>
2024-11-14 16:57:19 -0300

    Refactor AuthController to use MySQL connector and remove Database class dependency

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,23 +1,34 @@
 import bcrypt
 from models.usuario import Usuario
-from database import Database
+import mysql.connector
+from config.database import db_config
 
 
 class AuthController:
     def __init__(self):
-        self.db = Database()
+        self.db_config = db_config
+
+    def conectar(self):
+        return mysql.connector.connect(**self.db_config)
 
     def registrar_usuario(self, usuario, password):
         hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         usuario = Usuario(username=usuario, password_hash=hashed)
+        connection = self.conectar()
+        cursor = connection.cursor()
+
         query = "INSERT INTO Usuario (username, password_hash) VALUES (%s, %s)"
-        self.db.cursor.execute(query, (usuario.get_username(), hashed))
-        self.db.conn.commit()
-        self.db.close()
+
+        cursor.execute(query, (usuario.get_username(), hashed))
+        connection.commit()
+        cursor.close()
+        connection.close()
 
     def autenticar_usuario(self, usuario, password):
+        connection = self.conectar()
+        cursor = connection.cursor()
         query = "SELECT password_hash FROM Usuario WHERE username = %s"
-        self.db.cursor.execute(query, (usuario,))
+        cursor.execute(query, (usuario,))
         usuario = self.db.cursor.fetchone()
         if usuario:
             return bcrypt.checkpw(password.encode('utf-8'), usuario[0].encode('utf-8'))
