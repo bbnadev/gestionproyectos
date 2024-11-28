7706df58f07799e0a922f1cf4d9d53ad61030826
bbnadev <nicolas.bahamondeb@gmail.com>
2024-11-27 23:18:34 -0300

    Corrige error de sintaxis en la consulta SQL de inserción en indicadores_controller.py

--- controllers/indicadores_controller.py
+++ controllers/indicadores_controller.py
@@ -13,7 +13,7 @@ class IndicadorController:
     def crear(self, indicador: Indicador):
         conn = self.conectar()
         cursor = conn.cursor()
-        query = "INSERT INTO Indicadores (nombre, valor, usuario, fecha_registro) VALUES (%s, %s, %s. %s)"
+        query = "INSERT INTO Indicadores (nombre, valor, usuario, fecha_registro) VALUES (%s, %s, %s, %s)"
         cursor.execute(query, (indicador.get_nombre(),
                        indicador.get_valor(), indicador.get_usuario(), indicador.get_fecha_registro()))
         conn.commit()
@@ -21,14 +21,3 @@ class IndicadorController:
         cursor.close()
         conn.close()
         return id
-    # TODO
-
-# CREATE TABLE INDICADORES (
-# 	id INT PRIMARY KEY AUTO_INCREMENT,
-#     nombre varchar(25) NOT NULL,
-#     valor INT NOT NULL,
-#     usuario VARCHAR(255) NOT NULL,
-# 	sitio VARCHAR(255) DEFAULT "mindicador.cl",
-#     fecha_registro DATETIME NOT NULL,
-#     fecha_consulta DATETIME DEFAULT CURRENT_TIMESTAMP
-# )
